56b1f830c4bca498406c2a6b4256d4d71dba8c17
thienhihihihi <thien.cheviet1404@hcmut.edu.vn>
2024-10-07 11:33:07 +0700

    first commit

---
+++ Core/Inc/led_display.h
@@ -0,0 +1,19 @@
+/*
+ * led_display.h
+ *
+ *  Created on: Jul 25, 2023
+ *      Author: Phuc Le
+ */
+
+#ifndef __LED_DISPLAY_H_
+#define __LED_DISPLAY_H_
+
+#include "main.h"
+#include "timer.h"
+#include "global.h"
+
+void LedDispMode();
+void LEDScanning();
+void resetCountValue();
+
+#endif /* INC_LED_DISPLAY_H_ */
